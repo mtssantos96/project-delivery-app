3b10e10ba6d3da6fa2ad304e177a3f4c25f3c920
mtssantos96 <matheus.p.1996@hotmail.com>
2023-07-18 19:37:50 -0300

    feat: update app for deploy

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,13 +11,14 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
+  database: 
+    `${process.env.MYSQLDATABASE}${suffix[environment] || suffix.test}`,
   dialectOptions: {
     timezone: 'Z',
   },
